b02a9e2e79dabac082295f858b4bbb904a155fd9
McKai <echorecho@gmail.com>
2026-01-14 00:29:23 +0100

    Playing with some characters on screen

--- kernel.cpp
+++ kernel.cpp
@@ -1,68 +1,63 @@
-
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
-
-
-#define KEYBOARD_DATA_PORT 0x60
-#define KEYBOARD_STATUS_PORT 0x64
-
-
-extern "C" unsigned char inb(unsigned short port);
-
+typedef unsigned int uint32_t;
 
 class Terminal{
-	private:
-		uint16_t* buffer;
-		int cursor_x;
-		int cursor_y;
-		uint8_t color;
+		
+	public:	
+		uint16_t* screen_mem;
+		int offset;
+		int timer = 0;		
 
-	public:
-		Terminal(){
-			buffer = (uint16_t*)0xB8000;
-			cursor_x = 0;
-			cursor_y = 0;
-			color = 0x0A;
+		void init(){
+			screen_mem = (unsigned short*)0xB8000;
+			offset = 2;
 
-		}
-
-		void clear_scr(){
-			for(int i = 0; i < 80 * 25; ++i){   //80 columns 25 rows
-				buffer[i] = (uint16_t)' ' | (color << 8);  //shift left by 8 bites
+			for (int i = 0; i <= 80*25; ++i){
+				screen_mem[i] = ' ';
 			}
 		}
-
-		void put_char(char c){
-			if( c == '\n'){
-				cursor_x = 0;
-				cursor_y++;
-			}else{
-				int index = cursor_y * 80 + cursor_x;
-				buffer[index] = (uint16_t)c | (color << 8);
-				cursor_x++;
-			}
-			if (cursor_x >= 80){
-				cursor_x = 0;
-				cursor_y++;
-			}
+		
+		void print_char(char c){
+			screen_mem[offset] = (0x0F << 8) | c;
 		}
 
-		void write_text(const char* word){
-			for ( int i = 0; word[i] != '\0'; ++i ){
-				put_char(word[i]);
+		void printHex(unsigned int hex){
+			screen_mem[0] = (0x0F << 8) | '0';
+			screen_mem[1] = (0x0F << 8) | 'x';
+			const char* hexChars = "0123456789ABCDEF";
+
+			for ( int i = 0; i < 8; i++){
+				int digit = (hex >> (28 - i * 4) & 0xF);
+
+				char c = hexChars[digit];
+				print_char(c);
 			}
 		}
 };
 
+Terminal T;
 
-Terminal ter;
-void keyboard_driver_write();
-
+void flicker_char(char c){
+	T.print_char(c);
+	T.timer++;
+	if (T.offset >= 80*25){
+		T.offset = 0;
+	}
+	if (T.timer == 1000){
+		T.print_char(' ');
+		T.offset++;
+		T.timer = 0;
+	}
+}
 extern "C" void kernel_main() {
-    // Pointer to the VGA text buffer
- //   unsigned short* video_memory = (unsigned short*)0xB8000;
-	const char* kernel_message = "I/O Driver initialized!\t Type something \n";
-	ter = Terminal();
+	T.init();
+	T.offset = 0;
+	int i = 0;
+
+
+
+	
 /*
     for (int i = 0; i < 83; ++i) {
         // 0x0A is Light Green. 0x00 is Black background.
@@ -71,33 +66,17 @@ extern "C" void kernel_main() {
     }
 */
     
-    ter.clear_scr();
-    ter.write_text(kernel_message);
-    while(1) {
-	keyboard_driver_write();
+	const char* chars = "lsdkhpowefi23423589203490-12*&^#%$$!^#&$+=";
+   while(1){
 	
-    }
-
-}
-
-
-unsigned char keyboard_map[128] = {0,  27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b','\t','q', 'w', 'e', 'r','t', 'y', 'u', 'i', 'o', 'p', '[',']','\n', 0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', 0, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0, '*', 0, ' ', 0};	
-
-
-
-void keyboard_driver_write(){
-
-
-	if(inb(KEYBOARD_STATUS_PORT) & 0x01){
-		uint8_t scanned_code = inb(KEYBOARD_DATA_PORT);
-		if (!(scanned_code & 0x80)){
-			ter.put_char(keyboard_map[scanned_code]);
-		}	
+	flicker_char(chars[i]);
+	i++;
+	if ( i == 42){
+		i = 0;
 	}
+   }
+	
 }
-
-
-
 
 
 
